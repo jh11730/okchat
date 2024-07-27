334131837a72111ea2d76417bc6e33db231abb26
pjh <qkrwlsgur13@naver.com>
2024-07-27 22:27:12 +0900

    test

--- chat.js
+++ chat.js
@@ -1,17 +1,20 @@
-varsocket = io();
-varmessages = document.getElementById('messages');
-varform = document.getElementById('form');
-varinput = document.getElementById('input');
+var socket = io();
+
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+
 form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (input.value) {
-        socket.emit('chatmessage', input.value);
-        input.value = '';
-    }
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
 });
-socket.on('chatmessage', function (msg) {
-    varitem = document.createElement('li');
-    item.textContent = msg;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
+
+socket.on('chat message', function (msg) {
+  var item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
 });
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-constapp = require('express')();
-consthttp = require('http').Server(app);
-constio = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/chat.js', function (req, res) {
-    res.sendFile(__dirname + '/chat.js');
+  res.sendFile(__dirname + '/chat.js');
 });
 
 io.on('connection', function (socket) {
-    socket.on('chatmessage', function (msg) {
-        io.emit('chatmessage', msg);
-    });
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function () {
-    console.log('listeningon*:3000');
+  console.log('listening on *:3000');
 });
