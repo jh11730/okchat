76902689f21e005eb71c0b017a5a6bc73bc4eaed
pjh <qkrwlsgur13@naver.com>
2024-07-27 21:39:11 +0900

    server

---
+++ server.js
@@ -0,0 +1,21 @@
+constapp = require('express')();
+consthttp = require('http').Server(app);
+constio = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/chat.js', function (req, res) {
+    res.sendFile(__dirname + '/chat.js');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chatmessage', function (msg) {
+        io.emit('chatmessage', msg);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listeningon*:3000');
+});
